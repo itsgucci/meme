b6db9cdf5ac7f1b6e79e5477a36b9d1ec57608bd
itsgucci <drake@naturalfarminghawaii.net>
2015-08-11 00:26:40 -1000

    changed sort order and layout

--- generate_memes.rb
+++ generate_memes.rb
@@ -1,12 +1,15 @@
 
 
 def header
-  ""
+  "<html>"\
+  "<head>"\
+  "<style>img{clear:both}</style>"\
+  "</head>"
 end
 
 def body
   if filenames = Dir["memes/*.jpg"]
-    filenames.sort_by {|filename| File.mtime(filename) }.map {|filename| image(filename)}.join(" ")
+    filenames.sort_by {|filename| File.ctime(filename) }.map {|filename| image(filename)}.join(" ")
   else
     "<p>empty</p>"
   end
@@ -22,7 +25,7 @@ def image(filename)
 end
 
 def footer
-  ""
+  "</html>"
 end
 
 def overwrite_index(html)
