ef0340c881b35726108083b7e8a4ca95443a9d5b
itsgucci <drake@naturalfarminghawaii.net>
2015-08-11 00:10:53 -1000

    added site generator

---
+++ generate_memes.rb
@@ -0,0 +1,30 @@
+
+
+def header
+  ""
+end
+
+def body
+  if filenames = Dir["memes/*.jpg"]
+    filenames.sort_by {|filename| File.mtime(filename) }.map {|filename| image(filename)}.join(" ")
+  else
+    "<p>empty</p>"
+  end
+  
+end
+
+def image(filename)
+  "![me meme](memes/#{filename})"
+end
+
+def footer
+  ""
+end
+
+def overwrite_index(html)
+  File.open("index.html", "w") do |f|
+      f.write html
+  end
+end
+
+overwrite_index header + body + footer
